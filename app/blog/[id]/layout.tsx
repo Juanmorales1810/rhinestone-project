d9fe1250c5cfd05d583f124776485bbe71ecd64c
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2023-11-15 02:32:18 -0300

    responsive blog

--- app/blog/[id]/layout.tsx
+++ app/blog/[id]/layout.tsx
@@ -4,8 +4,8 @@ export default function BlogLayout({
     children: React.ReactNode;
 }) {
     return (
-        <section className="flex flex-col items-center justify-center gap-4 w-full h-[calc(100vh-116px)]">
-            <div className="inline-block text-center justify-center h-auto w-full px-2 md:px-0">
+        <section className="flex flex-col items-center justify-center gap-4 w-full h-full min-h-[calc(100vh-116px)]">
+            <div className="inline-block text-center justify-center h-auto w-full p-2 md:px-0">
                 {children}
             </div>
         </section>
